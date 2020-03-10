c54abed3c20cfda1b14dc6d0e35d02491bfa9fa7
alihelpervi <61271872+alihelpervi@users.noreply.github.com>
2020-03-10 11:10:27 +0200

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,13 +1,24 @@
 (function() {
-    var script = document.getElementById('mobileJS');
-    if (script == null) {
+    var wvWrapper = {};
+    wvWrapper.script = document.getElementById('mobileJS');
+    if (wvWrapper.script == null) {
         return;    
     }
     
-    var parent = document.getElementsByTagName('head').item(0);
-    var link = document.createElement('link');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.href = 'https://alihelpervi.github.io/mobile.css?' + script.dataset.ts;
-    parent.appendChild(link);
+    wvWrapper.cssTS = wvWrapper.script.dataset.ts;
+    
+    wvWrapper.injectCss = function(ts)  {
+        var parent = document.getElementsByTagName('head').item(0);
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = 'https://alihelpervi.github.io/mobile.css?' + ts;
+        parent.appendChild(link);
+    }
+    
+    wvWrapper.getTitle = function() {
+        return document.querySelector('#ms-title span').innerText;
+    }
+    
+    wvWrapper.injectCss(wvWrapper.cssTS);
 })();
