7a33dc598889e64b81b514a7bebd434a2b0ae4d4
alihelpervi <61271872+alihelpervi@users.noreply.github.com>
2020-03-10 10:39:28 +0200

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,8 +1,8 @@
 (function() {
     var parent = document.getElementsByTagName('head').item(0);
-    var style = document.createElement('link');
+    var link = document.createElement('link');
     link.type = 'text/css';
     link.rel = 'stylesheet';
     link.href = 'https://alihelpervi.github.io/mobile.css';
-    parent.appendChild(style);
+    parent.appendChild(link);
 })();
