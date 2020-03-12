0ded53ad0e1adba51eee8ce666236c51fc71b340
alihelpervi <61271872+alihelpervi@users.noreply.github.com>
2020-03-12 13:56:40 +0200

    Create mobile_dev.js

---
+++ mobile_dev.js
@@ -0,0 +1,30 @@
+(function() {
+    var script = document.getElementById('mobileJS');
+    if (script == null) {
+        return;    
+    }
+    
+    var cssTS = script.dataset.ts;
+    function injectCss(ts)  {
+        var parent = document.getElementsByTagName('head').item(0);
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = 'https://alihelpervi.github.io/mobile.css?' + ts;
+        parent.appendChild(link);
+    }
+    injectCss(cssTS);
+    
+    var webviewWrapper = {};
+    document.webviewWrapper = webviewWrapper;
+    webviewWrapper.getCategoryName = function() {
+        return document.querySelector('#ms-title span').innerText;
+    }
+    webviewWrapper.performSearch = function(query) {
+        AMP.setState({
+                        searchState: {
+                            inputValue: query
+                        }
+                    })
+    }
+})();
