2308263856d5362f50ad7c70108cd03549a9c4a4
alihelpervi <61271872+alihelpervi@users.noreply.github.com>
2021-06-11 17:58:29 +0300

    Update delivery_shein.js

--- delivery_shein.js
+++ delivery_shein.js
@@ -19,7 +19,6 @@
         var e=document.querySelector(".list-body");if(null==e)return null;var r=document.querySelector(".order-list-loading");if(null==r)return null;if("block"==r.style.display)return"loading";if(null!=document.querySelector("icon-empty-history"))return"empty";for(var t=[],l=[],i=e.getElementsByClassName("list-item-row"),s=0;s<i.length;s++){var a=i[s],o=a.querySelector(".c-icon-tip-spec>a");if(null!=o){var n=o.getAttribute("da-label"),u=n.split("-");if(2==u.length){var c=u[0].trim(),d="https://www.shein.com/user/orders/searchOrderGoods?limit=1&page=1&keyword="+c,p=a.querySelector(".list-con-img>img"),m="";null==p?l.push("fail to parse swiper-slide-active>img"):m="https:"+p.getAttribute("src");var y={orderId:c,trackUrl:d,imageUrl:m,title:c};t.push(y)}else l.push("fail to parse da-label: "+n)}else l.push("fail to find c-icon-tip-spec>a")}var f={data:t,errors:l};return JSON.stringify(f);
     }
     webviewWrapper.checkInvalidLogin = function() {
-//   return document.querySelector('.fm-error-message') != null
         return false;
     }
 })();
