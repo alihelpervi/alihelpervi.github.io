64bb645f7aa5bd4b2bf8383bf1dce015acc8b45d
alihelpervi <61271872+alihelpervi@users.noreply.github.com>
2022-01-04 13:47:41 +0200

    Update delivery_aliexpress.js

--- delivery_aliexpress.js
+++ delivery_aliexpress.js
@@ -40,7 +40,7 @@
         var e=document.querySelector(".order-list");if(null==e)return null;var t=document.querySelector("div[placeholder]");if(null==t)return null;if(!t.classList.contains("amp-hidden"))return"loading";if(null!=e.getElementsByClassName("ms-ept-page")[0])return"empty";for(var r=[],l=[],a=e.getElementsByClassName("order-item"),n=0;n<a.length;n++){var i=a[n],s=i.getElementsByClassName("order-item-id")[0];if(null!=s){var m=s.textContent,u=m.split(":");if(2==u.length){var o=u[1].trim(),d=i.getElementsByClassName("order-action")[0];if(null!=d){var g=d.getElementsByTagName("a")[0];if(null!=g){var f=g.getAttribute("href");if(f.includes("logistics")){var c=i.getElementsByTagName("img")[0];if(null!=c||null!=(c=i.getElementsByTagName("amp-img")[0])){var v=c.getAttribute("src"),p=i.getElementsByClassName("order-product-title")[0];if(null!=p){var y={orderId:o,trackUrl:f,imageUrl:v,title:p.textContent};r.push(y)}}}}}}else l.push("fail to parse orderId: "+m)}else l.push("fail to find order-item-id")}var N={data:r,errors:l};return JSON.stringify(N);
     }
     webviewWrapper.checkInvalidLogin = function() {
-        return document.querySelector('.fm-error-message') != null;
+        return document.querySelector('.fm-login .comet-alert-error') != null;
     }
     webviewWrapper.checkIsOrderPage = function() {
         var div = document.querySelector(".order-list");
